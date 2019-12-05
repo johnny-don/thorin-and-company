fdf1b835889dd16b2c98699eb561566e0ed2c3a4
Ubuntu <ubuntu@ip-172-31-44-119.ec2.internal>
2019-12-05 19:22:35 +0000

    fixed some bugs

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -16,25 +17,27 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
-    
-@app.route("/about/<member_name>")
+
+
+@app.route('/about/<member_name>')
 def about_member(member_name):
     member = {}
-    
+   
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
+   
     return render_template("member.html", member=member)
 
 
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        flash("Thanks {}, we have received your message!".format(request.form["name"]))
-      
-    
+        flash("Thanks {}, we have received your message".format(
+            request.form["name"]
+        ))
     return render_template("contact.html", page_title="Contact")
 
 
